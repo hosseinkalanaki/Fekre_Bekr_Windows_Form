cc182ce97bec0228dd4aa9a6d837adbcce021f2e
hossein kalanaki <hosseinkalanaki@gmail.com>
2019-02-17 00:49:22 +0330

    first upload

---
+++ FekreBekrWindowsForm/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace FekreBekrWindowsForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accept = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.withe = new System.Windows.Forms.RadioButton();
+            this.fuchsia = new System.Windows.Forms.RadioButton();
+            this.yellow = new System.Windows.Forms.RadioButton();
+            this.green = new System.Windows.Forms.RadioButton();
+            this.red = new System.Windows.Forms.RadioButton();
+            this.blue = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.roundButton4 = new FekreBekrWindowsForm.RoundButton();
+            this.roundButton3 = new FekreBekrWindowsForm.RoundButton();
+            this.roundButton2 = new FekreBekrWindowsForm.RoundButton();
+            this.roundButton1 = new FekreBekrWindowsForm.RoundButton();
+            this.SuspendLayout();
+            // 
+            // accept
+            // 
+            this.accept.BackColor = System.Drawing.Color.Peru;
+            this.accept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.accept.Location = new System.Drawing.Point(209, 466);
+            this.accept.Name = "accept";
+            this.accept.Size = new System.Drawing.Size(49, 42);
+            this.accept.TabIndex = 0;
+            this.accept.Text = "تایید";
+            this.accept.UseVisualStyleBackColor = false;
+            this.accept.Click += new System.EventHandler(this.accept_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.Peru;
+            this.clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clear.Font = new System.Drawing.Font("B Kamran", 14F);
+            this.clear.Location = new System.Drawing.Point(130, 466);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(49, 42);
+            this.clear.TabIndex = 1;
+            this.clear.Text = "تکرار";
+            this.clear.UseVisualStyleBackColor = false;
+            // 
+            // withe
+            // 
+            this.withe.AutoSize = true;
+            this.withe.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.withe.Location = new System.Drawing.Point(45, 443);
+            this.withe.Name = "withe";
+            this.withe.Size = new System.Drawing.Size(13, 12);
+            this.withe.TabIndex = 2;
+            this.withe.TabStop = true;
+            this.withe.UseVisualStyleBackColor = true;
+            this.withe.CheckedChanged += new System.EventHandler(this.withe_CheckedChanged);
+            // 
+            // fuchsia
+            // 
+            this.fuchsia.AutoSize = true;
+            this.fuchsia.BackColor = System.Drawing.Color.Fuchsia;
+            this.fuchsia.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.fuchsia.Location = new System.Drawing.Point(84, 443);
+            this.fuchsia.Name = "fuchsia";
+            this.fuchsia.Size = new System.Drawing.Size(13, 12);
+            this.fuchsia.TabIndex = 3;
+            this.fuchsia.TabStop = true;
+            this.fuchsia.UseVisualStyleBackColor = false;
+            // 
+            // yellow
+            // 
+            this.yellow.AutoSize = true;
+            this.yellow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.yellow.Location = new System.Drawing.Point(125, 443);
+            this.yellow.Name = "yellow";
+            this.yellow.Size = new System.Drawing.Size(13, 12);
+            this.yellow.TabIndex = 4;
+            this.yellow.TabStop = true;
+            this.yellow.UseVisualStyleBackColor = true;
+            // 
+            // green
+            // 
+            this.green.AutoSize = true;
+            this.green.BackColor = System.Drawing.Color.Lime;
+            this.green.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.green.Location = new System.Drawing.Point(165, 443);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(13, 12);
+            this.green.TabIndex = 5;
+            this.green.TabStop = true;
+            this.green.UseVisualStyleBackColor = false;
+            // 
+            // red
+            // 
+            this.red.AutoSize = true;
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.red.Location = new System.Drawing.Point(205, 443);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(13, 12);
+            this.red.TabIndex = 6;
+            this.red.TabStop = true;
+            this.red.UseVisualStyleBackColor = false;
+            // 
+            // blue
+            // 
+            this.blue.AutoSize = true;
+            this.blue.BackColor = System.Drawing.Color.Blue;
+            this.blue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.blue.Location = new System.Drawing.Point(245, 443);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(13, 12);
+            this.blue.TabIndex = 7;
+            this.blue.TabStop = true;
+            this.blue.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 27);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "به بازی فکر بکر خوش آمدید";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(241, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 27);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "آقا/خانم";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(171, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 27);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "حسین کلانکی";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(48, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 42);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "خروج";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // roundButton4
+            // 
+            this.roundButton4.BackColor = System.Drawing.Color.Black;
+            this.roundButton4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.roundButton4.Location = new System.Drawing.Point(241, 398);
+            this.roundButton4.Name = "roundButton4";
+            this.roundButton4.Size = new System.Drawing.Size(14, 15);
+            this.roundButton4.TabIndex = 19;
+            this.roundButton4.Text = "roundButton4";
+            this.roundButton4.UseVisualStyleBackColor = false;
+            // 
+            // roundButton3
+            // 
+            this.roundButton3.BackColor = System.Drawing.Color.Black;
+            this.roundButton3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.roundButton3.Location = new System.Drawing.Point(203, 398);
+            this.roundButton3.Name = "roundButton3";
+            this.roundButton3.Size = new System.Drawing.Size(14, 15);
+            this.roundButton3.TabIndex = 18;
+            this.roundButton3.Text = "roundButton3";
+            this.roundButton3.UseVisualStyleBackColor = false;
+            // 
+            // roundButton2
+            // 
+            this.roundButton2.BackColor = System.Drawing.Color.Black;
+            this.roundButton2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.roundButton2.Location = new System.Drawing.Point(165, 398);
+            this.roundButton2.Name = "roundButton2";
+            this.roundButton2.Size = new System.Drawing.Size(14, 15);
+            this.roundButton2.TabIndex = 17;
+            this.roundButton2.Text = "roundButton2";
+            this.roundButton2.UseVisualStyleBackColor = false;
+            this.roundButton2.Click += new System.EventHandler(this.roundButton2_Click);
+            // 
+            // roundButton1
+            // 
+            this.roundButton1.BackColor = System.Drawing.Color.Black;
+            this.roundButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.roundButton1.Location = new System.Drawing.Point(126, 398);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.Size = new System.Drawing.Size(14, 15);
+            this.roundButton1.TabIndex = 16;
+            this.roundButton1.Text = "roundButton1";
+            this.roundButton1.UseVisualStyleBackColor = false;
+            this.roundButton1.Click += new System.EventHandler(this.roundButton1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::FekreBekrWindowsForm.Properties.Resources.background1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(309, 517);
+            this.ControlBox = false;
+            this.Controls.Add(this.roundButton4);
+            this.Controls.Add(this.roundButton3);
+            this.Controls.Add(this.roundButton2);
+            this.Controls.Add(this.roundButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.yellow);
+            this.Controls.Add(this.fuchsia);
+            this.Controls.Add(this.withe);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.accept);
+            this.Font = new System.Drawing.Font("B Kamran", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button accept;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.RadioButton withe;
+        private System.Windows.Forms.RadioButton fuchsia;
+        private System.Windows.Forms.RadioButton yellow;
+        private System.Windows.Forms.RadioButton green;
+        private System.Windows.Forms.RadioButton red;
+        private System.Windows.Forms.RadioButton blue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private RoundButton roundButton1;
+        private RoundButton roundButton2;
+        private RoundButton roundButton3;
+        private RoundButton roundButton4;
+    }
+}
+
---
+++ FekreBekrWindowsForm/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FekreBekrWindowsForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public int Button1Location { get; set; }
+
+        //class ButtonColor
+        //{
+        //    public ButtonColor(string buttonname,string radiobuttonname)
+        //    {
+        //        Buttonname = buttonname;
+        //        Radiobuttonname = radiobuttonname;
+        //    }
+
+        //    private string Radiobuttonname;
+
+        //    private string buttonnname;
+
+        //    public string Buttonname
+        //    {
+        //        get
+        //        {
+        //            if (Radiobuttonname=="Red")
+        //            {
+                        
+        //            }
+        //            return buttonnname;
+        //        }
+        //        set
+        //        {
+        //            buttonnname = value;
+        //        }
+        //    }
+
+
+
+        //}
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Button1Location = 399;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void roundButton1_Click(object sender, EventArgs e)
+        {
+            if (blue.Checked == true)
+            {
+                roundButton1.BackColor = Color.Blue;
+            }
+            if (red.Checked == true)
+            {
+                roundButton1.BackColor = Color.Red;
+            }
+            if (green.Checked == true)
+            {
+                roundButton1.BackColor = Color.Green;
+            }
+            if (yellow.Checked == true)
+            {
+                roundButton1.BackColor = Color.Yellow;
+            }
+            if (fuchsia.Checked == true)
+            {
+                roundButton1.BackColor = Color.Fuchsia;
+            }
+            if (withe.Checked == true)
+            {
+                roundButton1.BackColor = Color.White;
+            }
+        }
+
+        private void withe_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roundButton2_Click(object sender, EventArgs e)
+        {
+            if (blue.Checked == true)
+            {
+                roundButton2.BackColor = Color.Blue;
+                
+            }
+            if (red.Checked == true)
+            {
+                roundButton2.BackColor = Color.Red;
+            }
+            if (green.Checked == true)
+            {
+                roundButton2.BackColor = Color.Green;
+            }
+            if (yellow.Checked == true)
+            {
+                roundButton2.BackColor = Color.Yellow;
+            }
+            if (fuchsia.Checked == true)
+            {
+                roundButton2.BackColor = Color.Fuchsia;
+            }
+            if (withe.Checked == true)
+            {
+                roundButton2.BackColor = Color.White;
+            }
+
+        }
+
+        private void accept_Click(object sender, EventArgs e)
+        {
+            if (accept.Text=="تایید")
+            {
+                roundButton1.Enabled = false;
+
+                roundButton2.Enabled = false;
+
+                roundButton3.Enabled = false;
+
+                roundButton4.Enabled = false;
+
+                accept.Text = "تکمیل";
+            }
+
+            else
+            {
+                roundButton1.Enabled = true;
+
+                roundButton1.Location = new Point(128, 349);
+
+                roundButton2.Enabled = true;
+
+                roundButton2.Location = new Point(166, 349);
+
+                roundButton3.Enabled = true;
+
+                roundButton3.Location = new Point(203, 349);
+
+                roundButton4.Enabled = true;
+
+                roundButton4.Location = new Point(241, 349);
+            }
+
+
+        }
+    }
+}
